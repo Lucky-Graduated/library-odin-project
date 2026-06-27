3c2f5795a3255a493726cf37dbfe40ebd4653b99
Lucky-Graduated <luckygraduated@gmail.com>
2026-06-27 12:50:16 +0530

    fix: styling

---
+++ script.js
@@ -0,0 +1,13 @@
+const 
+
+
+const cardContainer = document.createElement("div");
+const card = document.createElement("div");
+const image = document.createElement("div");
+const readBtn = document.createElement("div");
+const img = document.createElement("img");
+const card = document.createElement("div");
+const cardContainer = document.createElement("div");
+const card = document.createElement("div");
+const cardContainer = document.createElement("div");
+const card = document.createElement("div");
